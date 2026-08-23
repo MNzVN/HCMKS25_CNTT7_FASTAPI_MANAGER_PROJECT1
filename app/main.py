a372262bccb5ad446c9e6de6761792be4192086f
unknown <tranhieunghia004@gmail.com>
2026-08-24 02:13:41 +0700

    Day 2 - Task 6,7: Profile & user lists

--- app/main.py
+++ app/main.py
@@ -3,13 +3,14 @@ from fastapi.exceptions import RequestValidationError
 from app.db.database import engine, Base
 from app.core.exceptions import AppException, app_exception_handler, generic_exception_handler
 import app.models  # Nạp model để tự động tạo bảng
-from app.routers import auth
+from app.routers import auth, users
 
 # Tự động tạo bảng DB khi chạy
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Team Project Management API")
 app.include_router(auth.router)
+app.include_router(users.router)
 # Đăng ký các hàm xử lý lỗi đã viết bên app/core/exceptions.py
 app.add_exception_handler(AppException, app_exception_handler)
 app.add_exception_handler(Exception, generic_exception_handler)
